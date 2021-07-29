84b190c072a449354b2803a63e8f0949c4d8b9ba
navydragon <navydragon@inbox.ru>
2021-07-29 16:33:49 +0300

    29.07

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -365,7 +365,9 @@ class ExportController extends Controller
         $headFont = array('name' => 'Times New Roman','color' => '000000', 'size' => 14, 'bold' => true);
         $boldFont = array('name' => 'Times New Roman','color' => '000000', 'size' => 14, 'bold' => true);
         $normalFont = array('name' => 'Times New Roman','color' => '000000', 'size' => 14, 'bold' => false);
-        $littleTableFont = array('name' => 'Times New Roman','color' => '000000', 'size' => 10, 'bold' => false);
+        $tableLittleFont = array('name' => 'Times New Roman','color' => '000000', 'size' => 10, 'bold' => false);
+        $tableNormalFont = array('name' => 'Times New Roman','color' => '000000', 'size' => 12, 'bold' => false);
+        $tableBoldFont = array('name' => 'Times New Roman','color' => '000000', 'size' => 12, 'bold' => true);
         $redFont = array('name' => 'Times New Roman','color' => 'red', 'size' => 14, 'bold' => false);
         $titleParagraph = array('alignment' => 'both','lineHeight' => 1.5,'spaceAfter' => 0,'indentation'=> ['firstLine' => 708.661417323]);        
         $centerParagraph = array('alignment' => 'center','lineHeight' => 1.5,'spaceAfter' => 0);
@@ -385,15 +387,13 @@ class ExportController extends Controller
             'levels' => array(
             array('format' => 'bullet', 'text' => '–'),
             array('format' => 'bullet', 'text' => '–', 'left' => 720, 'hanging' => 360, 'tabPos' => 720),
-            )
-            )
-            );
+            )));
 
-        $tableStyle = array( 'borderColor' => '000000', 'borderSize'  => 1, 'cellMargin'  => 50,'width'=> '100%');
+        $tableStyle = array( 'borderColor' => '000000', 'borderSize'  => 1, 'cellMargin'  => 107.716535433,'width'=> '100%');
         $tableWOBordersStyle = array('cellMargin'  => 50,'width'=> '100%');
         
-        $firstRowStyle = array('bgColor' => '000');
-        $phpWord->addTableStyle('standart_table', $tableStyle, $firstRowStyle);
+        $firstRowStyle = array();
+        $phpWord->addTableStyle('standart_table', $tableStyle);
         $phpWord->addTableStyle('wo_borders_table', $tableWOBordersStyle, $firstRowStyle);
 
         /* Section */
@@ -448,17 +448,17 @@ class ExportController extends Controller
         for ($i = 0; $i < 10; $i++)
         {
             $table->addRow();
-            $cell=$table->addCell(3123,779527559);
-            $cell=$table->addCell(272,125984252);
-            $cell=$table->addCell(1094,173228346);
-            $cell=$table->addCell(272,125984252);
-            $cell=$table->addCell(4773,543307087);
+            $cell=$table->addCell(3123.779527559);
+            $cell=$table->addCell(272.125984252);
+            $cell=$table->addCell(1094.173228346);
+            $cell=$table->addCell(272.125984252);
+            $cell=$table->addCell(4773.543307087);
             $table->addRow();
-            $cell=$table->addCell(3123,779527559)->addText('ученое звание, ученая степень',$littleTableFont,$normalLineH1CenterParagraph);
-            $cell=$table->addCell(272,125984252);
-            $cell=$table->addCell(1094,173228346)->addText('подпись',$littleTableFont,$normalLineH1CenterParagraph);
-            $cell=$table->addCell(272,125984252);
-            $cell=$table->addCell(4773,543307087)->addText('ФИО',$littleTableFont,$normalLineH1CenterParagraph);
+            $cell=$table->addCell(3123.779527559)->addText('ученое звание, ученая степень',$tableLittleFont,$normalLineH1CenterParagraph);
+            $cell=$table->addCell(272.125984252);
+            $cell=$table->addCell(1094.173228346)->addText('подпись',$tableLittleFont,$normalLineH1CenterParagraph);
+            $cell=$table->addCell(272.125984252);
+            $cell=$table->addCell(4773.543307087)->addText('ФИО',$tableLittleFont,$normalLineH1CenterParagraph);
         }
 
         $section->addPageBreak();
@@ -468,7 +468,7 @@ class ExportController extends Controller
         /* 1.1.1 */ 
         $section->addTitle('1 Общая характеристика программы',0);
         $section->addTitle('1.1 Общие положения',1);
-        $section->addTitle('1.1.1 Нормативные правовые основания разработки',2);
+        $section->addText('1.1.1 Нормативные правовые основания разработки',$headFont,$titleParagraph);
         $section->addText('Нормативные правовые основания для разработки примерной дополнительной профессиональной программы – программы повышения квалификации «'.$dpp->name.'» (далее – программа) составляют:',$normalFont,$indentParagraph);
         $section->addListItem('Федеральный закон от 29 декабря 2012 г. № 273-ФЗ «Об образовании в Российской Федерации»;', 0, $normalFont,'multilevel_line' ,$indentParagraph);
         $section->addListItem('Федеральный закон от 03 июля 2016 г. № 238-ФЗ «О независимой оценке квалификации»;', 0, $normalFont,'multilevel_line' ,$indentParagraph);
@@ -477,8 +477,176 @@ class ExportController extends Controller
         $section->addListItem('приказ Минобрнауки России от 01 июля 2013 г. № 499 «Об утверждении Порядка организации и осуществления образовательной деятельности по дополнительным профессиональным программам»;', 0, $normalFont,'multilevel_line' ,$indentParagraph);
         $section->addListItem('приказ Минтруда России от 01 ноября 2016 г. № 601н «Об утверждении Положения о разработке оценочных средств для проведения независимой оценки квалификации».', 0, $normalFont,'multilevel_line' ,$indentParagraph);		
         
-
+        $section->addText('Программа разработана на основе профессионального стандарта, утвержденного приказом Минтруда России от 18 июля 2019 г. № 504н «Об утверждении профессионального стандарта 16.033 «Специалист в области планово-экономического обеспечения строительного производства».',$redFont,$indentParagraph);
+        $section->addText('Программа разработана на основе требований федерального государственного образовательного стандарта высшего образования по направлению подготовки бакалавров 38.03.01 «Экономика» (уровень бакалавриата), утвержденного приказом Минобрнауки России от 12 ноября 2015 г. № 1327, к результатам освоения образовательных программ.',$redFont,$indentParagraph);
+        $section->addText('Программа разработана на основе требований федерального государственного образовательного стандарта среднего профессионального образования по специальности 38.02.06 «Финансы», утвержденного приказом Минобрнауки России от 28 июля 2014 г. № 836, к результатам освоения образовательных программ.',$redFont,$indentParagraph);
         
+        /* 1.1.2 */
+        $section->addText('1.1.2 Требования к обучающимся',$headFont,$titleParagraph);
+        $section->addText('Требования к уровню профессионального образования:',$normalFont,$indentParagraph);
+        $section->addListItem('лица, имеющие высшее образование, лица, получающие высшее образование;', 0, $redFont,'multilevel_line' ,$indentParagraph);
+        $section->addListItem('лица, имеющие среднее профессиональное образование, лица, получающие среднее профессиональное образование', 0, $redFont,'multilevel_line' ,$indentParagraph);
+        $section->addText('Требование к квалификации: настоящая программа предназначена для повышения квалификации специалистов по ценообразованию и сметному делу в дорожном строительстве, включая инженеров-сметчиков, экономистов по договорной и претензионной работе, экономистов.',$redFont,$indentParagraph);
+        
+        /* 1.1.3 */
+        $section->addText('1.1.3 Форма обучения',$headFont,$titleParagraph);
+        $section->addText('Повышение квалификации может проводиться по выбору образовательной организации в соответствии с учебным планом в очной, очно-заочной или заочной формах обучения с применением дистанционных образовательных технологий и (или) электронного обучения',$normalFont,$indentParagraph);
+        /* 1.1.4 */
+        $section->addText('1.1.4 Трудоемкость освоения',$headFont,$titleParagraph);
+        $section->addText('24 ак. часа, включая все виды контактной и самостоятельной работы обучающегося.',$redFont,$indentParagraph);
+
+        /* 1.1.5 */
+        $section->addText('1.1.5 Срок освоения',$headFont,$titleParagraph);
+        $section->addText('3 календарных дня для очной формы обучения или 7 календарных дней для очно-заочной и заочной форм обучения с применением дистанционных образовательных технологий.',$redFont,$indentParagraph);
+        /* 1.2 */
+        $section->addTitle('1.2 Цель и задачи освоения',1);
+        /* 1.2.1 */
+        $section->addText('1.2.1 Цель освоения',$headFont,$titleParagraph);
+        $section->addText('Целью освоения программы являются совершенствование профессиональных компетенций работников дорожного хозяйства, необходимых для решения задач профессиональной деятельности при формировании сметной стоимости работ по капитальному ремонту автомобильных дорог, повышение профессионального уровня в рамках имеющейся квалификации в области профессиональной деятельности.',$normalFont,$indentParagraph);
+        
+        /* 1.2.2 */
+        $section->addText('1.2.2 Задачи освоения',$headFont,$titleParagraph);
+        $section->addText('Задачами освоения программы являются:',$normalFont,$indentParagraph);
+        $section->addListItem('приобретение обучающимися знаний, умений и навыков в соответствии с учебным планом и календарным графиком учебного процесса;', 0, $normalFont,'multilevel_line' ,$indentParagraph);
+        $section->addListItem('оценка достижений обучающимися планируемых результатов обучения.', 0, $normalFont,'multilevel_line' ,$indentParagraph);
+
+         /* 1.3 */
+        $section->addTitle('1.3 Планируемые результаты освоения, соотнесенные с планируемыми результатами обучения',1);
+        $section->addText('Таблица 1 – Планируемые результаты освоения:',$normalFont,$normalParagraph);
+        $table = $section->addTable('standart_table');
+        $table->addRow();
+        $cell=$table->addCell(702.992125984)->addText('Код',$tableBoldFont,$normalLineH1CenterParagraph);
+        $cell=$table->addCell(3089.763779528)->addText('Планируемые результаты освоения',$tableBoldFont,$normalLineH1CenterParagraph);
+        $cell=$table->addCell(5669.291338583)->addText('Планируемые результаты обучения',$tableBoldFont,$normalLineH1CenterParagraph);
+
+        /* 1.4 */
+        $section->addTitle('1.4 Учебный план ',1);
+        $section->addText('Таблица 2 – Учебный план',$normalFont,$normalParagraph);
+        $cellRowSpan = array('vMerge' => 'restart', 'valign' => 'center');
+        $cellRowContinue = array('vMerge' => 'continue');
+        $cellColSpan2 = array('gridSpan' => 2, 'valign' => 'center');
+        $cellColSpan3 = array('gridSpan' => 3, 'valign' => 'center');
+        $cellHCentered = array('align' => 'center');
+        $cellHCenteredNoSpace = array('align' => 'center','spaceAfter' => 0);
+        $cellNoSpace = array('spaceAfter' => 0,'lineHeight' => 1);
+        $cellAllCentered = array('align' => 'center', 'valign' => 'center','spaceAfter' => 0);
+        $cellVCentered = array('valign' => 'center');
+        $cellVerticalDirection = array('textDirection' => 'btLr','align' => 'center');
+        $cellRowSpanVerticalDirection = array('textDirection' => 'btLr','vMerge' => 'restart', 'valign' => 'center');
+
+        $table = $section->addTable('standart_table');
+        $table->addRow(null,array('tblHeader' => true));
+        $table->addCell(null,$cellRowSpan)->addText("Наименование разделов",$tableBoldFont,$cellHCentered);
+        $table->addCell(4200,['gridSpan' => 6])->addText("Трудоемкость, ак. час",$tableBoldFont,$cellHCenteredNoSpace);
+        $table->addCell(null,$cellRowSpan)->addText("Планируемые результаты обучения",$tableBoldFont,$cellHCentered);
+        $table->addRow(null,array('tblHeader' => true));
+        $table->addCell(null, $cellRowContinue); // пустая
+        $table->addCell(600, $cellRowSpanVerticalDirection)->addText('Итого', $tableBoldFont,$cellAllCentered);
+        $table->addCell(2250,['gridSpan' => 3])->addText("Виды занятий, в т.ч.",$tableBoldFont,$cellHCenteredNoSpace);
+        //$styleTable = array('borderSize' => 6, 'borderColor' => '999999');
+        $table->addCell(750, $cellRowSpanVerticalDirection)->addText('Самостоятельная работа', $tableBoldFont,$cellAllCentered);
+        $table->addCell(600, $cellRowSpanVerticalDirection)->addText('Итоговая аттестация', $tableBoldFont,$cellAllCentered);
+        $table->addCell(null, $cellRowContinue); // пустая
+       
+        $table->addRow(1800,array('tblHeader' => true));
+        $table->addCell(null, $cellRowContinue); // пустая
+        $table->addCell(null, $cellRowContinue); // пустая
+        $table->addCell(750,$cellVerticalDirection)->addText('лекционного типа', $tableBoldFont, $cellAllCentered);
+        $table->addCell(750,$cellVerticalDirection)->addText('практического типа', $tableBoldFont, $cellAllCentered);
+        $table->addCell(750,$cellVerticalDirection)->addText('лабораторная работа', $tableBoldFont, $cellAllCentered);
+        $table->addCell(null, $cellRowContinue); // пустая
+        $table->addCell(null, $cellRowContinue); // пустая	
+        $table->addCell(null, $cellRowContinue); // пустая
+        $sv = StructureVersion::find($dpp->st_version_id);
+        $sections = StructureSection::with(['knowledges' => function ($query) {}])
+         ->with(['themes' => function ($query) {$query->orderBy('position');}])
+         ->where('parent_id','=', null)
+         ->where('st_version_id','=', $sv->id)
+         ->orderBy('position')
+         ->get();
+         foreach ($sections as $sect)
+         {
+             $section->zuns = (object)['knowledges'=>$sect->knowledges,'abilities'=>$sect->abilities,'skills'=>$sect->skills];
+             foreach ($sect->themes as $theme)
+             {
+                $theme->zuns = (object)['knowledges'=>$theme->knowledges,'abilities'=>$theme->abilities,'skills'=>$theme->skills];
+             }
+         }
+        
+        foreach ($sections as $sect)
+        {
+            $table->addRow();
+            $table->addCell(null)->addText($sect->position." ".$sect->name,$tableBoldFont,$cellNoSpace);
+            $table->addCell(null)->addText($sect->total_hours,$tableBoldFont,$cellNoSpace);
+            $table->addCell(737)->addText($sect->lection_hours,$tableBoldFont,$cellNoSpace);
+            $table->addCell(737)->addText($sect->practice_hours,$tableBoldFont,$cellNoSpace);
+            $table->addCell(737)->addText($sect->lab_hours,$tableBoldFont,$cellNoSpace);
+            $table->addCell(null)->addText($sect->self_hours,$tableBoldFont,$cellNoSpace);
+            $table->addCell(null)->addText($sect->attestation_hours,$tableBoldFont,$cellNoSpace);
+            
+            $table_run=$table->addCell(null);
+            $table_run->addText('Знания:', $tableBoldFont,array('spaceAfter' => 0));
+            $kn_text = "";
+            $knowledges = $sect->knowledges;
+            foreach($knowledges as $key=>$value) {
+                $kn_text .=$value->name;
+                if ($key != count($knowledges)-1)
+                {
+                    $kn_text .="; ";
+                }
+            }
+            $table_run->addText($kn_text,$tableNormalFont);
+
+            //темы
+            foreach ($sect->themes as $theme)
+            {
+                $table->addRow();
+                $table->addCell(null)->addText($sect->position.".".$theme->position." ".$theme->name,$tableNormalFont,$cellNoSpace);
+                $table->addCell(null)->addText($theme->total_hours,$tableNormalFont,$cellHCentered,$cellNoSpace);
+                $table->addCell(null)->addText($theme->lection_hours,$tableNormalFont,$cellHCentered,$cellNoSpace);
+                $table->addCell(null)->addText($theme->practice_hours,$tableNormalFont,$cellHCentered,$cellNoSpace);
+                $table->addCell(null)->addText($theme->lab_hours,$tableNormalFont,$cellHCentered,$cellNoSpace);
+                $table->addCell(null)->addText($theme->self_hours,$tableNormalFont,$cellHCentered,$cellNoSpace);
+                $table->addCell(null)->addText($theme->attestation_hours,$tableNormalFont,$cellHCentered,$cellNoSpace);
+
+                $table_run=$table->addCell(null);
+                $table_run->addText('Знания:', $tableBoldFont,array('spaceAfter' => 0));
+                $kn_text = "";
+                $knowledges = $theme->knowledges;
+                foreach($knowledges as $key=>$value) {
+                    $kn_text .=$value->name;
+                    if ($key != count($knowledges)-1)
+                    {
+                        $kn_text .="; ";
+                    }
+                }
+            }
+        }
+
+         /* 1.5 */
+         $hours = $dpp->total_hours;
+         $o_days = ceil($hours / 8);
+         $o_width = 4500 / $o_days;
+         $z_days = ceil($hours / 4);
+         $z_width = 4500 / $z_days;
+         $section->addText('',$headFont,$titleParagraph);
+         $section->addTitle('1.5 Календарный учебный график',1);
+         $section->addText('Таблица 3 – Календарный учебный график для очной формы обучения',$normalFont,$normalParagraph);
+         $table = $section->addTable('standart_table');
+         $table->addRow(null,array('tblHeader' => true));
+         $table->addCell(null,$cellRowSpan)->addText("Наименование разделов",$tableBoldFont,$cellHCentered);
+         $table->addCell(4500,['gridSpan' => $o_days])->addText("Количество академических часов по дням",$tableBoldFont,$cellHCenteredNoSpace);
+         $table->addCell(1150,$cellRowSpan)->addText("ИТОГО",$tableBoldFont,$cellHCentered);
+         $table->addRow(null,array('tblHeader' => true));
+         $table->addCell(null, $cellRowContinue); // пустая
+         for ($i = 1; $i <= $o_days; $i++)
+         {
+            $table->addCell($o_width)->addText('Д'.$i, $tableBoldFont,$cellAllCentered);
+         }
+         $table->addCell(null, $cellRowContinue); // пустая 
+        
+        
+         
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
         try {
             $objWriter->save(storage_path('ПрДПП.docx'));
