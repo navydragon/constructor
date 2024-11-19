e32a5b1672c98ed6399e2e626ab5517f7c17e4b2
navydragon <navydragon@inbox.ru>
2024-11-19 13:51:39 +0300

    update

--- app/Http/Controllers/ExportOMController.php
+++ app/Http/Controllers/ExportOMController.php
@@ -423,7 +423,7 @@ class ExportOMController extends Controller
                         }
                         if ($answer->is_right == 1) {array_push($rigntAnswersArr,$symbols[$key]);}
                     }
-                    if (($number > 350)&&($number < 375)) {
+                    if (($number > 375)&&($number < 400)) {
                         $t->setComplexBlock('answers#'.$number, $table);
                     }
 
